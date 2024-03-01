fdcee3ae8226e0bdb8637b1cb033198dddd8a95d
JohnEsleyer <ralphesleyer@gmail.com>
2024-03-01 17:20:52 +0800

    added animations and icon

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { ChangeEvent, useState } from "react";
+import Image from 'next/image';
 
 export interface GeneratedText {
   meaning: string;
@@ -29,19 +30,18 @@ export default function Home() {
 
   const [inputValue, setInputValue] = useState<string>('');
   const [isVisible, setIsVisible] = useState<boolean>(false);
-
-  const toggleVisibility = () => {
-    setIsVisible(!isVisible);
-  }
-
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  // const [didUpdate, setDidUpdate] = useState<boolean>(false);
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+
 
     setIsVisible(false);
     setInputValue(e.target.value);
   };
 
   const handleClick = async () => {
-
+    setIsVisible(false);
+    setIsLoading(true);
     const res = await fetch('/api/japan', {
       method: 'POST',
       body: JSON.stringify({ message: inputValue }),
@@ -52,15 +52,23 @@ export default function Home() {
 
     const generatedText: GeneratedText = await res.json();
     setGeneratedText(generatedText);
-    toggleVisibility();
+    setIsLoading(false);
+    setIsVisible(true);
 
   }
 
   return (
     <>
       <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white font-sans">
-        <div className="max-w-md p-6 rounded-lg m-16 shadow-lg bg-gray-800">
-          <h1 className="text-4xl font-bold mb-4">Mochi</h1>
+        <div className={`${isLoading ? "shimmer-effect" : ""} max-w-md p-6 rounded-lg m-16 shadow-lg w-4/5 bg-gray-800`}>
+        <div className="flex items-center justify-center">
+        <Image src="/mochi.png" width={100} height={100} alt="mochi"/>
+          <div className="flex items-center grid">
+          <h1 className="text-4xl font-bold mt-4"><span className="text-yellow-500">M</span><span className="text-pink-500">o</span><span className="text-green-500">c</span>h<span className="text-orange-500">i</span></h1>
+          <span className="text-sm">Japanese AI Text Analyzer</span>
+          {/* <span className="text-xs">Made by <a className="text-orange-300"href="https://www.github.com/johnesleyer">@JohnEsleyer</a></span> */}
+          </div>
+          </div>
           <input
             type="text"
             value={inputValue}
@@ -68,16 +76,20 @@ export default function Home() {
             placeholder="Type word or phrase"
             className="w-full p-2 mb-4 rounded bg-gray-700 text-white"
           />
-          <button
-            onClick={handleClick}
-            className="w-full p-2 mb-4 rounded bg-blue-500 hover:bg-blue-600 transition duration-300"
-          >
-            Analyze
-          </button>
+          
+          {isLoading ? <p></p> : <button
+          
+          onClick={handleClick}
+          className="w-full p-2 mb-4 rounded text-black bg-orange-200 transition duration-300"
+        >
+          Analyze Text
+        </button>}
+          
           <h3 className="text-xl font-semibold mb-2"></h3>
 
+
           <div className={`transition-opacity transition-height duration-500 ease-in-out overflow-hidden ${
-          isVisible ? 'opacity-100 max-h-full' : 'opacity-0 max-h-0'
+          isVisible ? 'opacity-100 max-h-full' : `${isLoading ? 'opacity-0 max-h-full' : 'opacity-0 max-h-0'}`
         }`}>
           <span className="font-bold text-xl mr-4">"{inputValue}"</span>
 
