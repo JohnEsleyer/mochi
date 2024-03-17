3e1fa259623d7dc26fb2b0d5e8025e876ddea80c
JohnEsleyer <ralphjohnpolicarpio@outlook.com>
2024-03-17 14:45:54 +0800

    update chat

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -121,11 +121,11 @@ export default function ChatAI() {
                             {" " + "Server is overloaded with requests, please try again later"}
                             <button onClick={tryAgain} className="rounded text-xs p-1 text-black bg-orange-200 transition duration-300">Try Again</button></span>}</p>
                     <p className="text-lg">{message}</p>
-                    <div className="p-2">
+                    {index % 2 == 0 && <div className="p-2">
                     {isFurigana && <p>Furigana: <span className="text-purple-300">{conversationHiragana[index]}</span></p>}
                     {isRomaji &&<p>Romaji: <span className="text-green-300">{conversationRomaji[index]}</span></p>}
                     {isEnglishTranslate && <p>English: <span className="text-amber-200">{conversationEnglish[index]}</span></p>}
-                    </div>
+                    </div>}
                 </div>
             ))}
             </div>
