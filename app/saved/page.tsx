c357ca45ac1e743498b260bac14b42b7ea2cc125
JohnEsleyer <ralphjohnpolicarpio@outlook.com>
2024-04-19 23:29:52 +0800

    display saved analysed texts per user

--- app/saved/page.tsx
+++ app/saved/page.tsx
@@ -8,7 +8,30 @@ interface SavedTextPreviewProps {
     meaning: string;
 }
 
-const SavedTextPreview: React.FC<SavedTextPreviewProps> = ({text, meaning}) => {
+interface Words{
+    word: string;
+    romaji: string;
+    class: string;
+    meaning: string;
+    context: string;
+}
+
+interface Body {
+    japanese: string;
+    meaning: string;
+    furigana: string;
+    romaji: string;
+    context: string;
+    words: Record<string, Words>;
+}
+
+interface JsonData {
+    lang: string;
+    body: Body;
+}
+
+
+const SavedTextPreview: React.FC<SavedTextPreviewProps> = ({ text, meaning }) => {
     return (
         <div className="border border-white ">
             <p>{text}</p>
@@ -17,59 +40,97 @@ const SavedTextPreview: React.FC<SavedTextPreviewProps> = ({text, meaning}) => {
     );
 }
 
+interface SaveData {
+    created_at: string;
+    id: number;
+    text: string
+}
 
 async function getSavedData() {
 
-    let { data: saved, error } = await supabase
-    .from('saved')
-    .select('*')
-    
-    if (error){
-     
+    let { data, error } = await supabase
+        .from('saved')
+        .select('*')
+
+    const saveData = data as SaveData[];
+    if (error) {
+        console.log(error);
         throw error;
     }
-
-    return saved;
+    
+    console.log(saveData[0].text);
+    
+    
+    return saveData;
 }
 
-export default function Katakana(){
+
+export default function Katakana() {
+
 
     const [isError, setIsError] = useState(false);
-
+    const [userJsonData, setUserJsonData] = useState<JsonData[]>([]);
+    const [userSavedData, setUserSavedData] = useState<SaveData[]>([]);
 
     useEffect(() => {
+
+        try {
+
+            const savedData = getSavedData();
+
+            if (savedData !== null) {
+                console.log(savedData.then((value) => {
+                    setUserSavedData(value);
+                }));
+            }
+
+            let temp: JsonData[] = [];
+            savedData.then((value) => {
+                value.forEach((value2, index) => {
+                    temp.push(JSON.parse(value2.text) as JsonData);
+                });
+            });
+
+            setUserJsonData(temp);
+
         
-       try{
-        const saved = getSavedData();
-        console.log(saved);
-       }catch(e){
-        console.log(e);
-        setIsError(true);
-       }
-                
+          
+
+        } catch (e) {
+            console.log(e);
+            setIsError(true);
+        }
+
     }, []);
 
     return (
         <Template>
-            <div className="h-full grid grid-cols-6 bg-blue-300">
-            <div className="col-span-2 lg:col-span-1 bg-gray-900 p-2 pt-4">
-                <p>Groups</p>
-                <div className="grid grid-cols-3 p-2 bg-orange-200 text-black rounded">
-                <p className="col-span-2">New group</p>
-                <span className="col-span-1 material-symbols-outlined flex justify-end">
-                    add_box
-                </span>
+            <div className="h-full grid grid-cols-6">
+                <div className="col-span-2 lg:col-span-1 bg-gray-900 p-2 pt-4">
+                    <p>Groups</p>
+                    <div className="grid grid-cols-3 p-2 bg-orange-200 text-black rounded">
+                        <p className="col-span-2">New group</p>
+                        <span className="col-span-1 material-symbols-outlined flex justify-end">
+                            add_box
+                        </span>
+                    </div>
                 </div>
-            </div>
-            <div className="col-span-4 lg:col-span-5 p-5 bg-green-300">
-                <p className="text-2xl font-bold bg-green-300">Recent</p>
-                <div className="flex-1 grid grid-cols-3 bg-yellow-300">
-                    <SavedTextPreview text="こんにちは" meaning="Hello" />
+                <div className="col-span-4 lg:col-span-5 p-5">
+                    <p className="text-2xl font-bold">Recent</p>
+                    <div className="flex-1 grid grid-cols-3">
+                        <SavedTextPreview text="こんにちは" meaning="Hello" />
+                        {
+                            userJsonData.map((value, index) => (
+                               
+                                <SavedTextPreview text={value.body.japanese} meaning={value.body.meaning} />
+                            ))
+                        }
+                    </div>
+
                 </div>
-                
+
             </div>
-            </div>
-            
-            </Template>
-    );  
+
+        </Template>
+    );
 }
